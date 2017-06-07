9e7aac2581f340509c13142f12f005cbe4dbc2f8
Warsama ILMI AMIR <warsama.ilmiamir@gmail.com>
2017-06-07 15:18:12 +0200

    init Spring Boot

---
+++ src/main/java/dev/exemplespringboot/ExempleSpringBootApplication.java
@@ -0,0 +1,12 @@
+package dev.exemplespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExempleSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExempleSpringBootApplication.class, args);
+	}
+}
---
+++ src/test/java/dev/exemplespringboot/ExempleSpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package dev.exemplespringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExempleSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
